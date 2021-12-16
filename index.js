3634c5f02937c648ad6eb8e5cad7558d91e37be6
Tate Frost <tatefrost96@gmail.com>
2021-12-16 11:45:30 -0700

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ddbb0e8bfaa84bb3921110805c7bb126',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`server is running from the law on ${port}`))
